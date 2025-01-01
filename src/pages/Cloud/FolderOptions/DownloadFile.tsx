d6bf869533e0bc49b3a62586e41d265f6320db2d
Egrenbido Oyintare Paul <paulinholapaulinho@gmail.com>
2025-01-01 19:19:25 +0100

    added download functionality, though not perfect

---
+++ src/pages/Cloud/FolderOptions/DownloadFile.tsx
@@ -0,0 +1,33 @@
+import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
+import { getFileURL } from "@/firebase/services";
+import { FileType, Folder } from "@/types";
+import { Download } from "lucide-react";
+
+const DownloadFile = ({
+  resource,
+  closeDropdown,
+}: {
+  resource: FileType | Folder;
+  closeDropdown: () => void;
+}) => {
+  const handleDownload = async () => {
+    const url = await getFileURL(resource.path);
+    const anchor = document.createElement("a");
+    anchor.href = url;
+    anchor.target = "_blank";
+    anchor.download = resource.name;
+    anchor.click();
+    anchor.type = resource.mimeType;
+    closeDropdown();
+  };
+  return (
+    <DropdownMenuItem
+      className="flex gap-2 items-center"
+      onClick={handleDownload}
+    >
+      <Download className="size-4" />
+      Download
+    </DropdownMenuItem>
+  );
+};
+export default DownloadFile;
--- src/pages/Cloud/FolderOptions/index.tsx
+++ src/pages/Cloud/FolderOptions/index.tsx
@@ -9,6 +9,7 @@ import { EllipsisVertical } from "lucide-react";
 import MoveToTrash from "./MoveToTrash";
 import AddToFavourites from "./AddToFavourites";
 import { FileType, Folder } from "@/types";
+import DownloadFile from "./DownloadFile";
 
 const FolderOptions = ({ resource }: { resource: FileType | Folder }) => {
   const [open, setOpen] = useState(false);
@@ -29,6 +30,10 @@ const FolderOptions = ({ resource }: { resource: FileType | Folder }) => {
             favourite={resource.favourite}
           />
         )}
+        {resource.type == "file" && (
+          <DownloadFile resource={resource} closeDropdown={closeDropdown} />
+        )}
+
         <DropdownMenuItem>
           <MoveToTrash id={resource.id} closeDropdown={closeDropdown} />
         </DropdownMenuItem>
